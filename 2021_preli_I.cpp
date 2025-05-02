03ad0ed162cb6019734a3da2122acafa5201e902
Nupur-gitt <146472606+Nupur-gitt@users.noreply.github.com>
2025-05-02 22:09:48 +0600

    Geometry

--- 2021_preli_I.cpp
+++ 2021_preli_I.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 #define ll long long
+#define all(v) v.begin(),v.end()
 #define fast()  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define yes     cout<<"YES"<<endl
 #define no      cout<<"NO"<<endl
